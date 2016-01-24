24cd2c2d191fb566fd181a7c52522739280e501c
phrounz <phrounz@gmail.com>
2016-01-24 18:34:42 +0100

    Changed (unused) get_sdl2

--- common/JS_Emscripten/get_sdl2.bat
+++ common/JS_Emscripten/get_sdl2.bat
@@ -1,4 +1,4 @@
-C:\Users\moi\AppData\Local\GitHub\PortableGit_c7e0cbde92ba565cb218a521411d0e854079a28c\cmd\git clone https://github.com/gsathya/SDL-emscripten.git
+curl -sL https://github.com/emscripten-ports/SDL2/archive/master.zip | unzip 
 cd SDL-emscripten/
 emconfigure ./configure --host=asmjs-unknown-emscripten --disable-assembly --disable-threads --enable-cpuinfo=false CFLAGS="-O2"
 emmake make
